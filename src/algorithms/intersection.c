e3f18434397f7e9b650fdb988164077b816be6e9
Mike <miguelciencias@mike-gaspar.(none)>
2014-04-21 23:56:24 -0500

    ahoritita subo el readme

--- src/algorithms/intersection.c
+++ src/algorithms/intersection.c
@@ -11,5 +11,22 @@
 struct double_linked_list* find_intersections(struct double_linked_list*
 											  half_edges)
 {
-	return NULL;
+	struct double_linked_list* intersections;
+	struct list_item *tmp1, *tmp2;
+		
+	tmp1 = tmp2 = half_edges->head;
+	intersections = init_double_linked_list(POINT);
+		
+	while(tmp1 != NULL){
+		while(tmp2 != NULL){
+			if(he_intersection(tmp1->element, tmp2->element) != NULL){
+				push_back(intersections, he_intersection(tmp1->element, tmp2->element));
+				tmp2 = tmp2->right;
+			}else{
+				tmp2 = tmp2->right;
+			}
+		}
+		tmp1 = tmp1->right;
+	}
+	return intersections;
 }
